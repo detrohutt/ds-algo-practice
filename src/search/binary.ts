0c2c318ea575aaa996406692adae7e13055b728f
A.J. Roberts <detrohutt@gmail.com>
2018-08-18 06:16:54 -0400

    Remove unnecessary code from binary search implementation

--- src/search/binary.ts
+++ src/search/binary.ts
@@ -1,11 +1,10 @@
 import { SearchFn } from "./types";
 
 const binarySearchInner = (arr: number[], term: number, left: number, right: number): number => {
-  if (arr.length === 0 || left > right) return -1;
+  if (left > right) return -1;
   else if (arr.length === 1) return arr[0] === term ? 0 : -1;
 
   const mid = Math.floor((right - left) / 2) + left;
-
   if (arr[mid] > term) return binarySearchInner(arr, term, left, mid - 1);
   else if (arr[mid] < term) return binarySearchInner(arr, term, mid + 1, right);
   else return mid;
